a1dccdd467a4eafd6941ef51b2cd9037f544e666
jay <wjay.inclino@gmail.com>
2024-11-27 07:59:57 +0800

    update TRIP_TICKET_STATUS enum values should match APPROVAL_STATUS enum

--- lvis-api/apps/warehouse/src/trip-ticket/entities/trip-ticket.enums.ts
+++ lvis-api/apps/warehouse/src/trip-ticket/entities/trip-ticket.enums.ts
@@ -4,7 +4,7 @@ export const enum TRIP_TICKET_STATUS {
     PENDING = 1,
     APPROVED = 2,
     DISAPPROVED = 3,
-    IN_PROGRESS = 4,
-    COMPLETED = 5,
-    CANCELLED = 6,
+    CANCELLED = 4,
+    IN_PROGRESS = 5,
+    COMPLETED = 6,
 }
