b69b66c54bfcf287c85e0a5e6d00c06b6536df5f
jay <wjay.inclino@gmail.com>
2024-11-28 17:49:16 +0800

    setup prometheus module in api-gateway

--- lvis-api/apps/api-gateway/src/app.module.ts
+++ lvis-api/apps/api-gateway/src/app.module.ts
@@ -7,6 +7,7 @@ import { verify } from 'jsonwebtoken'
 import { AuthModule } from './auth/auth.module';
 import { FileUploadModule } from './file-upload-warehouse/file-upload.module';
 import { FileUploadSystemModule } from './file-upload-system/file-upload-system.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 // import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 const getToken = (authToken: string) => {
@@ -96,7 +97,7 @@ function handleAuth({ req }) {
     AuthModule,
     FileUploadModule,
     FileUploadSystemModule,
-    // PrometheusModule.register()
+    PrometheusModule.register()
   ],
   controllers: [],
   providers: [],
