eda8ff280f20700e8f96a2628e16ea7d3b8342ab
jay <wjay.inclino@gmail.com>
2025-02-06 11:55:31 +0800

    Fix UI mrv add or update items

--- lvis-web/components/Warehouse/AddItemSearch.vue
+++ lvis-web/components/Warehouse/AddItemSearch.vue
@@ -3,38 +3,21 @@
         <div class="card-body">
 
             <div class="row align-items-center">
-                <!-- Search By Dropdown -->
-                <div class="col-lg-3 col-md-12 col-sm-12 p-1">
-                    <button class="btn btn-primary dropdown-toggle w-100" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Search by {{ searchBy }}
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a @click="handleSearchByChange(SEARCH_BY.CODE)" class="dropdown-item" href="javascript:void(0)">
-                                Code
-                            </a>
-                        </li>
-                        <li>
-                            <a @click="handleSearchByChange(SEARCH_BY.DESC)" class="dropdown-item" href="javascript:void(0)">
-                                Description
-                            </a>
-                        </li>
-                    </ul>
-                </div>
 
                 <!-- Search Input -->
-                <div class="col-lg-8 col-md-12 col-sm-12 p-1">
+                <div class="col-10 p-1">
                     <client-only>
                         <v-select 
                             :options="filteredItems" 
                             label="label" 
                             v-model="selectedItem"
+                            placeholder="Search here..."
                         ></v-select>
                     </client-only>
                 </div>
 
                 <!-- Add Button -->
-                <div class="col-lg-1 col-md-12 col-sm-12 p-1">
+                <div class="col-2 p-1">
                     <button :disabled="!selectedItem" @click="handleAddItem()" class="btn btn-success w-100" type="button">
                         <client-only>
                             <font-awesome-icon :icon="['fas', 'plus']" />
