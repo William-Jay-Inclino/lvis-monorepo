96bdf84a004b087584c87c3efcba58319ca69b21
jay <wjay.inclino@gmail.com>
2025-02-07 10:15:21 +0800

    fix bug in mst item resolver remove setAuthUser

--- lvis-api/apps/warehouse/src/mst-item/mst-item.resolver.ts
+++ lvis-api/apps/warehouse/src/mst-item/mst-item.resolver.ts
@@ -41,11 +41,10 @@ export class MstItemResolver {
 
         try {
             
-            this.mstItemService.setAuthUser(authUser)
-      
             const x = await this.mstItemService.updateMstItems(mst_id, items, {
                 ip_address,
-                device_info: this.audit.getDeviceInfo(user_agent)
+                device_info: this.audit.getDeviceInfo(user_agent),
+                authUser,
             });
             
             this.logger.log('MST Items updated successfully')
